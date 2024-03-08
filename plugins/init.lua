7c0c2aa0f9ce604f5771d76c677c65d928ca77a4
Bassam Sayed <bassam.sayed@worldcoin.org>
2024-03-08 18:38:04 -0500

    Added format-on-save pluging and config.

--- plugins/init.lua
+++ plugins/init.lua
@@ -258,6 +258,82 @@ local plugins = {
       require("autoclose").setup()
     end,
   },
+  {
+    "elentok/format-on-save.nvim",
+    config = function()
+      local format_on_save = require("format-on-save")
+      local formatters = require("format-on-save.formatters")
+      format_on_save.setup({
+        exclude_path_patterns = {
+        "/node_modules/",
+        ".local/share/nvim/lazy",
+      },
+      formatter_by_ft = {
+        css = formatters.lsp,
+        html = formatters.lsp,
+        java = formatters.lsp,
+        json = formatters.lsp,
+        lua = formatters.lsp,
+        markdown = formatters.prettierd,
+        openscad = formatters.lsp,
+        rust = formatters.lsp,
+        scad = formatters.lsp,
+        scss = formatters.lsp,
+        sh = formatters.shfmt,
+        terraform = formatters.lsp,
+        typescript = formatters.prettierd,
+        typescriptreact = formatters.prettierd,
+        yaml = formatters.lsp,
+
+        -- Add your own shell formatters:
+        myfiletype = formatters.shell({ cmd = { "myformatter", "%" } }),
+
+        -- Add lazy formatter that will only run when formatting:
+        my_custom_formatter = function()
+          if vim.api.nvim_buf_get_name(0):match("/README.md$") then
+            return formatters.prettierd
+          else
+            return formatters.lsp()
+          end
+        end,
+
+        -- Add custom formatter
+        filetype1 = formatters.remove_trailing_whitespace,
+        filetype2 = formatters.custom({ format = function(lines)
+          return vim.tbl_map(function(line)
+            return line:gsub("true", "false")
+          end, lines)
+        end}),
+
+        -- Concatenate formatters
+        python = {
+          formatters.remove_trailing_whitespace,
+          formatters.shell({ cmd = "tidy-imports" }),
+          formatters.black,
+          formatters.ruff,
+        },
+
+        -- Use a tempfile instead of stdin
+        go = {
+          formatters.shell({
+            cmd = { "goimports-reviser", "-rm-unused", "-set-alias", "-format", "%" },
+            tempfile = function()
+              return vim.fn.expand("%") .. '.formatter-temp'
+            end
+          }),
+          formatters.shell({ cmd = { "gofmt" } }),
+        },
+      },
+
+        -- Optional: fallback formatter to use when no formatters match the current filetype
+        fallback_formatter = {
+          formatters.remove_trailing_whitespace,
+          formatters.remove_trailing_newlines,
+          formatters.prettierd,
+        },
+      })
+    end,
+  },
 }
 
 return plugins
